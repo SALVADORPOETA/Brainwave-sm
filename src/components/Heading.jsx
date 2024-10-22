4408ad5fcd4f7e0e67cb09a2990476409f95dc74
unknown <dsml@live.com>
2024-10-22 01:50:30 -0600

    test10

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,11 +1,11 @@
-// import Tagline from "./Tagline";
+import Test from "./Test";
 
 const Heading = ({ className, title, text, tag }) => {
   return (
     <div
       className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20 md:text-center`}
     >
-      {/* {tag && <Tagline className="mb-4 md:justify-center">{tag}</Tagline>} */}
+      {tag && <Test className="mb-4 md:justify-center">{tag}</Test>}
       {title && <h2 className="h2">{title}</h2>}
       {text && <p className="body-2 mt-4 text-n-4">{text}</p>}
     </div>
